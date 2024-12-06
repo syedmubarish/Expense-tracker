e4b4623fe4254c3cfcac412caafd24851c39c478
mubarish <mubarish007@gmail.com>
2024-12-06 12:32:45 +0530

    pass data via props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css'
+function Expenseitem(props){
+
+    const expenseDate = props.date
+    const expenseTitle = props.title
+    const expenseAmount = props.amount
+    return(
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+            </div>
+        </div>
+    )
+}
+export default Expenseitem;
